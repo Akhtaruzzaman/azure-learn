14fcdd8d25916e86a13e761ef657bd0a410b89b1
Md. Akhtar Uzzaman <akhtaruzzaman05@gmail.com>
2022-01-10 11:29:25 +0600

    upd

---
+++ AzureFunctionDem/GitHumMonitorApp/GitHubMonitor.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace GitHumMonitorApp
+{
+    public static class GitHubMonitor
+    {
+        [FunctionName("GitHubMonitor")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed an action.");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            //Todo - Do something with the data
+            log.LogInformation(requestBody);
+
+            return new OkResult();
+        }
+    }
+}
+
+// {firstName:"MD", lastName:"Akhtar"}
